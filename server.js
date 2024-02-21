9d4ccdc9d6f1abe2ddfe131c505f9993ca7466dc
zdotdev <www.ragaszedrick25@gmail.com>
2024-02-21 23:44:49 +0800

    remove comments in server.js

--- server.js
+++ server.js
@@ -5,30 +5,7 @@ import courseRouter from "./routes/router.js";
 
 const app = express()
 app.use(cors())
-
 app.use(express.json())
-
-// const courseSchema = new mongoose.Schema({});
-
-// const course = mongoose.model("course", courseSchema)
-
-// mongoose.connect("mongodb://localhost:27017/courses") // /courses to access the specific database
-// .then(() => {
-//     app.listen(3000)
-//     app.get("/", async(req, res) => {
-//         try{
-//             const data = await course.find();
-//             res.status(200).json(data)
-//         }
-//         catch(err){
-//             console.log(err)
-//         }
-//     })
-// })
-// .catch((err) => {
-//     console.log(err)
-// })
-
 app.use("/api", courseRouter)
 
 mongoose.connect("mongodb://localhost:27017/courses")
